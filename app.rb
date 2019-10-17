a41270133e865ba466cea1b35cde23c60314ee9e
Randy Taylor <randyisbusy@gmail.com>
2019-10-17 19:14:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do
     enable :sessions
-      set :session_secrete
+      set :session_secret
     end
      get '/' do 
        @session = session
